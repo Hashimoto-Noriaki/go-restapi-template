b4a70842ac532f1b0f471d8942794e291fc93f2a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-01 00:54:59 +0900

    トークン発行

--- app/services/auth_service.go
+++ app/services/auth_service.go
@@ -3,7 +3,10 @@ package services
 import (
 	"go-restapi-boiltertemplate/app/models"
 	"go-restapi-boiltertemplate/app/repositories"
+	"time"
+	"os"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -43,6 +46,23 @@ func (s *AuthService) Login(email string, password string) (*string, error){
 	if err != nil {
 		return nil, err
 	}
+	token, err := CreateToken(foundUser.ID, foundUser.Email)
+	if err != nil {
+		return nil, err
+	}
+	// トークンを返す
+	return token, nil
+}
+func CreateToken(userId uint, email string)(*string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":   userId,
+		"email": email,
+		"exp":   time.Now().Add(time.Hour).Unix(),
+	})
 
-	return &foundUser.Email,nil
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		return nil, err
+	}
+	return &tokenString, nil
 }
